e824e03aecca558b3c20c6823bb48a5682d2647f
cjames222 <carltonjam@gmail.com>
2022-07-21 23:11:15 -0400

    Modified App.js

--- pokemon-generations-starters/src/App.js
+++ pokemon-generations-starters/src/App.js
@@ -2,10 +2,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Some Content
+    <div className='wrapper'>
+      <div className='accordion'>
+
+        {data.map((item, i) => {
+          <div className='item'>
+            <div className='title'>
+              <h2></h2>
+            </div>
+          </div>
+        })}
+      </div>
     </div>
   );
 }
+
+const data =[]
 
 export default App;
