30d733f24dc7d59fbbdae699f65abc800853898a
Carlton James Jr <cjames222@Genayas-MacBook-Air.local>
2022-07-26 12:10:49 -0400

    Added background and button

--- pokemon-generations-starters/src/App.js
+++ pokemon-generations-starters/src/App.js
@@ -23,73 +23,55 @@ const App = () => {
                 <header>GENERATION STARTERS</header>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 1</h2>
-                <span>+</span>
+                <h2 className="gen-header">GENERATION 1<button>+</button></h2>
                 <div className="wrapper">
                     <First />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 2</h2>
-                <span>+</span>
+                <h2 className="gen-header">GENERATION 2<button>+</button></h2>
                 <div className="wrapper">
                     <Second />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 3</h2>
-                <span>+</span>
+                <h2 className="gen-header">GENERATION 3<button>+</button></h2>
                 <div className="wrapper">
                     <Third />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 4</h2>
-                <span>+</span>
+                <h2 className="gen-header">GENERATION 4<button>+</button></h2>
                 <div className="wrapper">
                     <Forth />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 5</h2>
-                <span>+</span>
+                <h2 className="gen-header">GENERATION 5<button>+</button></h2>
                 <div className="wrapper">
                     <Fifth />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 6</h2>
-                <span>+</span>
+                <h2 className="gen-header">GENERATION 6<button>+</button></h2>
                 <div className="wrapper">
                     <Sixth />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 7</h2>
-                <span>+</span>
+                <h2 className="gen-header">GENERATION 7<button>+</button></h2>
                 <div className="wrapper">
                     <Seventh />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 8</h2>
-                <span>+</span>
+                <h2 className="gen-header">GENERATION 8<button>+</button></h2>
                 <div className="wrapper">
                     <Eighth />
                 </div>
             </div>
         </div>
     )
-    
-    //return (
-        //<IconContext.Provider value={{color: '#00FFB9', size: '25px'}}>
-            //<AccordionSection>
-                //<container>
-                    //<h1>test</h1>
-                //</container>
-            //</AccordionSection>
-        //</IconContext.Provider>
-    //)
 }
 
 export default App
