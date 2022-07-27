6423d1cea8606cf74e7773b169bc52fc3cdf1fff
Carlton James Jr <cjames222@Genayas-MacBook-Air.local>
2022-07-27 10:03:46 -0400

    Made important changes

--- pokemon-generations-starters/src/App.js
+++ pokemon-generations-starters/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import First from './components/First'
 import Second from './components/Second'
 import Third from './components/Third'
@@ -9,11 +9,6 @@ import Seventh from './components/Seventh'
 import Eighth from './components/Eighth'
 import CSS from './App.css'
 import styled from 'styled-components'
-import {IconContext} from 'react-icons'
-import {FiPlus, FiMinus} from 'react-icons/fi'
-
-const AccordionSection = styled.div
-const Container = styled.div
 
 const App = () => {
     
@@ -23,49 +18,49 @@ const App = () => {
                 <header>GENERATION STARTERS</header>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 1<button>+</button></h2>
+                <h2 className="gen-header">GENERATION 1<button className="accordion_button">+</button></h2>
                 <div className="wrapper">
                     <First />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 2<button>+</button></h2>
+                <h2 className="gen-header">GENERATION 2<button className="accordion_button">+</button></h2>
                 <div className="wrapper">
                     <Second />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 3<button>+</button></h2>
+                <h2 className="gen-header">GENERATION 3<button className="accordion_button">+</button></h2>
                 <div className="wrapper">
                     <Third />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 4<button>+</button></h2>
+                <h2 className="gen-header">GENERATION 4<button className="accordion_button">+</button></h2>
                 <div className="wrapper">
                     <Forth />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 5<button>+</button></h2>
+                <h2 className="gen-header">GENERATION 5<button className="accordion_button">+</button></h2>
                 <div className="wrapper">
                     <Fifth />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 6<button>+</button></h2>
+                <h2 className="gen-header">GENERATION 6<button className="accordion_button">+</button></h2>
                 <div className="wrapper">
                     <Sixth />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 7<button>+</button></h2>
+                <h2 className="gen-header">GENERATION 7<button className="accordion_button">+</button></h2>
                 <div className="wrapper">
                     <Seventh />
                 </div>
             </div>
             <div className="generation">
-                <h2 className="gen-header">GENERATION 8<button>+</button></h2>
+                <h2 className="gen-header">GENERATION 8<button className="accordion_button">+</button></h2>
                 <div className="wrapper">
                     <Eighth />
                 </div>
